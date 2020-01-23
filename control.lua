2aad1ed2e67ad31692be45a6836bd8d0762aaa9e
IQuick 143 <IQuick143cz@gmail.com>
2020-01-23 17:35:28 +0100

    created a simple test mod for RCON

---
+++ control.lua
@@ -0,0 +1,8 @@
+script.on_event({defines.events.on_tick},
+    function (e)
+        if e.tick % 30 == 0 then --common trick to reduce how often this runs, we don't want it running every tick, just every 30 ticks, so twice per second
+			print("Foo")
+			game.print("Bar")
+        end
+    end
+)
