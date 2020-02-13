2a5c94444a502308240969e035426b66f7a3b935
IQuick 143 <IQuick143cz@gmail.com>
2020-02-13 16:22:25 +0100

    First working pre-alpha

--- tools/run.bat
+++ tools/run.bat
@@ -1,2 +1,1 @@
-D:\SteamLibrary\steamapps\common\Factorio\bin\x64\factorio.exe --map-gen-seed 413 --start-server-load-scenario base/freeplay -c config.ini
-D:\SteamLibrary\steamapps\common\Factorio\bin\x64\factorio.exe --mp-connect 127.0.0.1
+D:\SteamLibrary\steamapps\common\Factorio\bin\x64\factorio.exe --map-gen-seed 413 --start-server-load-scenario base/freeplay -c config.ini --rcon-port 2050 --rcon-password RCONPASS
